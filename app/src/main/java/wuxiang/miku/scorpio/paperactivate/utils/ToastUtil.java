7092e221ed2f5dec50d810dd79a72a71b96ba8d7
ScorpioMiku <1056992492@qq.com>
2018-05-02 11:44:50 +0800

    1

--- app/src/main/java/wuxiang/miku/scorpio/paperactivate/utils/ToastUtil.java
+++ app/src/main/java/wuxiang/miku/scorpio/paperactivate/utils/ToastUtil.java
@@ -7,12 +7,6 @@ import android.widget.Toast;
 
 
 
-/**
- * Created by hcc on 16/8/4 21:18
- * 100332338@qq.com
- * <p/>
- * Toast工具类
- */
 public class ToastUtil {
     public static void showShort(Context context, String text) {
         Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
